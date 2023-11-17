30b1de2abeefafdc2230db1debb1a6db773b8707
Everix23 <32474476+Everix23@users.noreply.github.com>
2023-11-16 16:17:24 -0800

    CHANGES

--- cli.py
+++ cli.py
@@ -76,16 +76,94 @@ class MyCLI(cmd.Cmd):
             if self.functions.active_table:
                 records = parts[1:]
                 dbtable = self.functions.active_table
+                chunk_size = 500
                 self.functions.add_records_to_table(records,dbtable)
             else:
                 print("No active table. Use '! table dbtable' to set the active table.")
         elif len(parts) >= 3 and parts[0] == '=':
-            column = parts[1]
-            value = parts[2]
-            dbtable = parts[3] if len(parts) >= 4 else self.functions.active_table
-            self.functions.search_in_folder(column, value, dbtable)
+            search_column = parts[1]
+            search_value = parts[2]
+
+            # Determine if a specific table is specified and where the display columns start
+            dbtable = self.functions.active_table
+            display_columns_start = 3
+            if len(parts) > 3 and parts[3].lower() != 'show':
+                dbtable = parts[3]
+                display_columns_start = 4
+
+            # Check if specific columns to display are specified
+            display_columns = None
+            if len(parts) > display_columns_start and parts[display_columns_start].lower() == 'show':
+                display_columns = parts[display_columns_start + 1:]
+
+            self.functions.search_in_folder(search_column, search_value, dbtable, display_columns)
         elif len(parts) == 2 and parts[0] == 'show' and parts[1] == 'active':
             self.functions.show_active()
+        elif len(parts) == 2 and parts[0] == '=':
+            dbtable = parts[1]
+            self.functions.show_entire_table(dbtable)
+        elif len(parts) >= 3 and parts[0] == '-' and parts[1] == '=':
+            column = parts[2]
+            value = parts[3]
+            dbtable = self.functions.active_table if len(parts) == 4 else parts[4]
+            self.functions.delete_records(column, value, dbtable)
+        elif len(parts) == 2 and parts[0] == '#':
+            dbtable = parts[1] if len(parts) > 1 else self.functions.active_table
+            count = self.functions.count_records(dbtable)
+            print(f"Total records in '{dbtable}': {count}")
+        elif len(parts) >= 2 and parts[0] == '*':
+            column = parts[1]
+            dbtable = parts[2] if len(parts) > 2 else self.functions.active_table
+            total_sum = self.functions.sum_numeric_column(dbtable, column)
+            if total_sum is not None:
+                print(f"Sum of numeric values in column '{column}' of '{dbtable}': {total_sum}")
+            else:
+                print(f"Column '{column}' contains non-numeric values or is empty.")
+        elif len(parts) == 2 and parts[0] == '*/#':
+            column = parts[1]
+            dbtable = self.functions.active_table
+            average = self.functions.average_numeric_column(dbtable, column)
+            if average is not None:
+                print(f"Average of numeric values in column '{column}' of '{dbtable}': {average}")
+        elif len(parts) >= 2 and parts[0] == '^':
+            column = parts[1]
+            dbtable = parts[2] if len(parts) > 2 else self.functions.active_table
+            max_value = self.functions.find_max_in_column(dbtable, column)
+            if max_value is not None:
+                print(f"Maximum value in column '{column}' of '{dbtable}': {max_value}")
+            else:
+                print(f"Could not determine the maximum value in column '{column}' of '{dbtable}'.")
+        elif len(parts) >= 2 and parts[0] == '_':
+            column = parts[1]
+            dbtable = parts[2] if len(parts) > 2 else self.functions.active_table
+            min_value = self.functions.find_min_in_column(dbtable, column)
+            if min_value is not None:
+                print(f"Minimum value in column '{column}' of '{dbtable}': {min_value}")
+            else:
+                print(f"Could not determine the minimum value in column '{column}' of '{dbtable}'.")
+        elif line.startswith("[] "):
+            _, column = line.split(maxsplit=1)
+            if self.functions.active_table:
+                # Group by column
+                self.functions.group_by_column(self.functions.active_table, column)
+
+                # Print and delete folder
+                self.functions.print_and_delete_folder(f"{self.functions.active_table}_groupby_{column}")
+
+            else:
+                print("No active table. Use '! table dbtable' to set the active table.")
+
+        elif line.startswith("@ "):
+            _, column = line.split(maxsplit=1)
+            if self.functions.active_table:
+                # Order by column
+                self.functions.order_by_column(self.functions.active_table, column)
+
+                # Print and delete folder
+                self.functions.print_and_delete_folder(f"{self.functions.active_table}_orderby_{column}")
+
+            else:
+                print("No active table. Use '! table dbtable' to set the active table.")
         else:
             print(f"Unknown syntax: {line}")
 if __name__ == "__main__":
--- functions.py
+++ functions.py
@@ -87,25 +87,21 @@ class MyFunctions:
                 # Process the last chunk (if any)
                 if chunk_rows:
                     folder_path = os.path.join(dbtable, f"{dbtable}_chunk_{chunk_number}.csv")
-                    self.process_chunk(folder_path, chunk_rows, header)
+                    self.process_chunk(folder_path, chunk_rows, header, first_chunk=(chunk_number == 0))
 
             print(f"Data loaded into chunks within folder '{dbtable}'.")
 
-    def process_chunk(self, folder_path, chunk_rows, header):
-        with open(folder_path, 'w', newline='') as folder_csv:
-            csvwriter = csv.writer(folder_csv)
-            if header:
-                csvwriter.writerow(header)  # Write the header in every chunk
-            csvwriter.writerows(chunk_rows)
+    def process_chunk(self, chunk_path, chunk_data, header):
+        with open(chunk_path, 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(header)
+            csvwriter.writerows(chunk_data)
 
     def show_active(self):
         print(f"Active Database: {self.active_database}")
         print(f"Active Table: {self.active_table}")
     def add_records_to_table(self, records, dbtable):
         def get_num_columns(table_path):
-            # Implement the logic to get the number of columns in the table
-            # You can read the header of the first chunk file to determine the columns
-            # For simplicity, I'm assuming that the first chunk file contains the header
             first_chunk_path = os.path.join(table_path, f"{table_path}_chunk_1.csv")
             if os.path.exists(first_chunk_path):
                 with open(first_chunk_path, 'r') as chunk_csv:
@@ -121,7 +117,7 @@ class MyFunctions:
             last_chunk_path = self.get_last_chunk_path(folder_path)
             if last_chunk_path:
                 last_chunk_size = os.path.getsize(last_chunk_path)
-                chunk_size_limit = 5000
+                chunk_size_limit = 500
 
                 # Check if the number of records matches the number of columns
                 num_columns = get_num_columns(dbtable)
@@ -153,12 +149,25 @@ class MyFunctions:
             return None
 
     def create_new_chunk(self, folder_path, records):
-        chunk_number = len([f for f in os.listdir(folder_path) if f.endswith(".csv")]) + 1
-        chunk_path = os.path.join(folder_path, f"{chunk_number}.csv")
+        def get_header(table_path):
+            first_chunk_path = os.path.join(table_path, next(os.walk(table_path))[2][0])
+            if os.path.exists(first_chunk_path):
+                with open(first_chunk_path, 'r') as chunk_csv:
+                    csvreader = csv.reader(chunk_csv)
+                    header = next(csvreader, None)
+                    return header
+            return []
+
+        header = get_header(folder_path)
+        chunk_number = len([f for f in os.listdir(folder_path) if f.endswith(".csv")])
+        chunk_path = os.path.join(folder_path, f"{self.active_table}_chunk_{chunk_number}.csv")
 
         with open(chunk_path, 'w', newline='') as chunk_csv:
             csvwriter = csv.writer(chunk_csv)
+            if header:
+                csvwriter.writerow(header)
             csvwriter.writerow(records)
+
 
         return chunk_path
 
@@ -168,13 +177,14 @@ class MyFunctions:
             csvwriter.writerow(records)
 
         print(f"Records appended to the last chunk: {chunk_path}")
-    def search_in_folder(self, column, value, dbtable=None):
+    def search_in_folder(self, column, value, dbtable=None, display_columns=None):
         if not dbtable:
             dbtable = self.active_table
 
         folder_path = f"{dbtable}"
         if os.path.exists(folder_path):
             matching_records = []
+            found_columns = []
 
             for file_name in os.listdir(folder_path):
                 if file_name.endswith(".csv"):
@@ -183,18 +193,319 @@ class MyFunctions:
                         csvreader = csv.reader(csvfile)
                         header = next(csvreader, None)
 
-                        if column in header:
-                            column_index = header.index(column)
+                        if header:
+                            if column not in header:
+                                print(f"Column '{column}' does not exist in the table.")
+                                return
+
+                            if display_columns:
+                                found_columns = [col for col in display_columns if col in header]
+                                if not found_columns:
+                                    print("None of the specified display columns exist in the table.")
+                                    return
+                                display_indexes = [header.index(col) for col in found_columns]
+                            else:
+                                display_indexes = range(len(header))
 
                             for row in csvreader:
-                                if row[column_index] == value:
-                                    matching_records.append(row)
+                                if row[header.index(column)] == value:
+                                    matching_records.append([row[i] for i in display_indexes])
 
             if matching_records:
-                print(f"Matching records in '{dbtable}':")
+                if found_columns:
+                    print(f"Matching records in '{dbtable}' (Columns: {', '.join(found_columns)}):")
+                else:
+                    print(f"Matching records in '{dbtable}':")
+
                 for record in matching_records:
                     print(record)
             else:
                 print(f"No matching records found in '{dbtable}'.")
         else:
             print(f"Table '{dbtable}' does not exist.")
+    def show_entire_table(self, dbtable):
+        folder_path = f"{dbtable}"
+        if os.path.exists(folder_path) and os.path.isdir(folder_path):
+            for file_name in os.listdir(folder_path):
+                if file_name.endswith(".csv"):
+                    file_path = os.path.join(folder_path, file_name)
+                    with open(file_path, 'r', newline='') as csvfile:
+                        csvreader = csv.reader(csvfile)
+                        for row in csvreader:
+                            print(', '.join(row))
+        else:
+            print(f"Table '{dbtable}' does not exist or is not a directory.")
+    def delete_records(self, column, value, dbtable):
+        folder_path = f"{dbtable}"
+        if os.path.exists(folder_path) and os.path.isdir(folder_path):
+            for file_name in os.listdir(folder_path):
+                if file_name.endswith(".csv"):
+                    file_path = os.path.join(folder_path, file_name)
+                    with open(file_path, 'r', newline='') as csvfile:
+                        rows = list(csv.reader(csvfile))
+
+                    # Check if the column exists in the table
+                    header = rows[0] if rows else []
+                    if column not in header:
+                        print(f"Column '{column}' does not exist in '{dbtable}'.")
+                        continue
+
+                    column_index = header.index(column)
+                    updated_rows = [row for row in rows if row[column_index] != value]
+
+                    # Write the updated rows back to the CSV file
+                    with open(file_path, 'w', newline='') as csvfile:
+                        csvwriter = csv.writer(csvfile)
+                        csvwriter.writerows(updated_rows)
+
+            print(f"Records where '{column}' equals '{value}' have been deleted from '{dbtable}'.")
+        else:
+            print(f"Table '{dbtable}' does not exist or is not a directory.")
+
+    def sum_numeric_column(self, dbtable, column):
+        folder_path = f"{dbtable}"
+        total_sum = 0
+        max_decimal_places = 0
+        is_numeric = True
+        first_file = True
+
+        if not os.path.exists(folder_path) or not os.path.isdir(folder_path):
+            print(f"Table '{dbtable}' does not exist or is not a directory.")
+            return None
+
+        for file_name in os.listdir(folder_path):
+            if file_name.endswith(".csv"):
+                file_path = os.path.join(folder_path, file_name)
+                with open(file_path, 'r', newline='') as csvfile:
+                    csvreader = csv.reader(csvfile)
+                    header = next(csvreader, None)
+                    if first_file and column not in header:
+                        print(f"Column '{column}' does not exist in '{dbtable}'.")
+                        return None
+                    first_file = False
+
+                    col_index = header.index(column)
+
+                    for row in csvreader:
+                        try:
+                            value_str = row[col_index]
+                            value = float(value_str)
+                            total_sum += value
+
+                            # Determine the number of decimal places
+                            if '.' in value_str:
+                                decimal_places = len(value_str.split('.')[1])
+                                max_decimal_places = max(max_decimal_places, decimal_places)
+                        except ValueError:
+                            is_numeric = False
+                            break
+
+        if is_numeric:
+            # Adjust the total sum for floating point precision based on max_decimal_places
+            return round(total_sum, max_decimal_places)
+        else:
+            return None
+
+    def count_records(self, dbtable):
+        folder_path = f"{dbtable}"
+        record_count = 0
+
+        if not os.path.exists(folder_path) or not os.path.isdir(folder_path):
+            print(f"Table '{dbtable}' does not exist or is not a directory.")
+            return 0
+
+        for file_name in os.listdir(folder_path):
+            if file_name.endswith(".csv"):
+                file_path = os.path.join(folder_path, file_name)
+                with open(file_path, 'r', newline='') as csvfile:
+                    csvreader = csv.reader(csvfile)
+                    next(csvreader, None)  # Skip the header row
+                    for row in csvreader:
+                        record_count += 1
+
+        return record_count
+    def average_numeric_column(self, dbtable, column):
+        total_sum = self.sum_numeric_column(dbtable, column)
+        if total_sum is not None:
+            record_count = self.count_records(dbtable)
+            if record_count > 0:
+                average = total_sum / record_count
+                return average
+            else:
+                print(f"No records found in '{dbtable}'.")
+                return None
+        else:
+            print(f"Column '{column}' contains non-numeric values or is empty.")
+            return None
+    def find_max_in_column(self, dbtable, column):
+        folder_path = f"{dbtable}"
+        max_value = None
+        is_numeric = True
+        first_file = True
+
+        if not os.path.exists(folder_path) or not os.path.isdir(folder_path):
+            print(f"Table '{dbtable}' does not exist or is not a directory.")
+            return None
+
+        for file_name in os.listdir(folder_path):
+            if file_name.endswith(".csv"):
+                file_path = os.path.join(folder_path, file_name)
+                with open(file_path, 'r', newline='') as csvfile:
+                    csvreader = csv.reader(csvfile)
+                    header = next(csvreader, None)
+                    if first_file and column not in header:
+                        print(f"Column '{column}' does not exist in '{dbtable}'.")
+                        return None
+                    first_file = False
+
+                    col_index = header.index(column)
+
+                    for row in csvreader:
+                        try:
+                            value = float(row[col_index])
+                            if max_value is None or value > max_value:
+                                max_value = value
+                        except ValueError:
+                            is_numeric = False
+                            break
+
+        if is_numeric:
+            return max_value
+        else:
+            return None
+    def find_min_in_column(self, dbtable, column):
+        folder_path = f"{dbtable}"
+        min_value = None
+        is_numeric = True
+        first_file = True
+
+        if not os.path.exists(folder_path) or not os.path.isdir(folder_path):
+            print(f"Table '{dbtable}' does not exist or is not a directory.")
+            return None
+
+        for file_name in os.listdir(folder_path):
+            if file_name.endswith(".csv"):
+                file_path = os.path.join(folder_path, file_name)
+                with open(file_path, 'r', newline='') as csvfile:
+                    csvreader = csv.reader(csvfile)
+                    header = next(csvreader, None)
+                    if first_file and column not in header:
+                        print(f"Column '{column}' does not exist in '{dbtable}'.")
+                        return None
+                    first_file = False
+
+                    col_index = header.index(column)
+
+                    for row in csvreader:
+                        try:
+                            value = float(row[col_index])
+                            if min_value is None or value < min_value:
+                                min_value = value
+                        except ValueError:
+                            is_numeric = False
+                            break
+
+        if is_numeric:
+            return min_value
+        else:
+            return None
+    def group_by_column(self, dbtable, column, chunk_size=500):
+        folder_path = dbtable
+        if not os.path.exists(folder_path) or not os.path.isdir(folder_path):
+            print(f"Table '{dbtable}' does not exist or is not a directory.")
+            return
+
+        grouped_data = {}
+        header = None
+
+        for file_name in os.listdir(folder_path):
+            if file_name.endswith(".csv"):
+                file_path = os.path.join(folder_path, file_name)
+                with open(file_path, 'r', newline='') as csvfile:
+                    csvreader = csv.reader(csvfile)
+
+                    if header is None:
+                        header = next(csvreader, None)
+                        col_index = header.index(column)
+                    else:
+                        next(csvreader, None)  # Skip header in subsequent files
+
+                    for row in csvreader:
+                        key = row[col_index]
+                        grouped_data.setdefault(key, []).append(row)
+
+        new_folder = f"{dbtable}_groupby_{column}"
+        os.makedirs(new_folder, exist_ok=True)
+
+        for key, rows in grouped_data.items():
+            # Chunking grouped data
+            for i in range(0, len(rows), chunk_size):
+                chunk_data = rows[i:i + chunk_size]
+                chunk_path = os.path.join(new_folder, f"group_{key}_{i // chunk_size}.csv")
+                with open(chunk_path, 'w', newline='') as csvfile:
+                    csvwriter = csv.writer(csvfile)
+                    csvwriter.writerow(header)
+                    csvwriter.writerows(chunk_data)
+
+        print(f"Grouped data saved in chunks in '{new_folder}'")
+
+    def order_by_column(self, dbtable, column, chunk_size=500):
+        folder_path = dbtable
+        if not os.path.exists(folder_path) or not os.path.isdir(folder_path):
+            print(f"Table '{dbtable}' does not exist or is not a directory.")
+            return
+
+        all_data = []
+        header = None
+
+        for file_name in os.listdir(folder_path):
+            if file_name.endswith(".csv"):
+                file_path = os.path.join(folder_path, file_name)
+                with open(file_path, 'r', newline='') as csvfile:
+                    csvreader = csv.reader(csvfile)
+
+                    if header is None:
+                        header = next(csvreader, None)
+                        col_index = header.index(column)
+                    else:
+                        next(csvreader, None)  # Skip header in subsequent files
+
+                    all_data.extend(csvreader)
+
+        all_data.sort(key=lambda x: x[col_index])
+
+        new_folder = f"{dbtable}_orderby_{column}"
+        os.makedirs(new_folder, exist_ok=True)
+
+        for i in range(0, len(all_data), chunk_size):
+            chunk_data = all_data[i:i + chunk_size]
+            chunk_path = os.path.join(new_folder, f"chunk_{i // chunk_size}.csv")
+            with open(chunk_path, 'w', newline='') as csvfile:
+                csvwriter = csv.writer(csvfile)
+                if i == 0:
+                    csvwriter.writerow(header)  # Header only in the first chunk
+                csvwriter.writerows(chunk_data)
+
+        print(f"Ordered data saved in chunks in '{new_folder}'")
+
+    def print_and_delete_folder(self, folder_path):
+        if os.path.exists(folder_path) and os.path.isdir(folder_path):
+            for file_name in sorted(os.listdir(folder_path)):
+                file_path = os.path.join(folder_path, file_name)
+                if file_name.endswith(".csv"):
+                    # Print file contents
+                    with open(file_path, 'r', newline='') as csvfile:
+                        csvreader = csv.reader(csvfile)
+                        for row in csvreader:
+                            print(', '.join(row))
+
+                    # Delete file
+                    #os.remove(file_path)
+
+            # Delete folder after all files are deleted
+            #os.rmdir(folder_path)
+            print(f"Folder '{folder_path}' has been deleted.")
+        else:
+            print(f"Folder '{folder_path}' does not exist.")
+
+    
